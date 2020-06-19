0cc453c0049ae96deb54ef0362f16200c228f2b9
Omar De Leo <omar.deleo@gmail.com>
2020-06-19 14:07:22 -0400

    Create SignIn component

---
+++ frontend/src/SignIn.js
@@ -0,0 +1,103 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState("");
+  const [error, setError] = React.useState(false);
+  const errorMessage = error
+    ? "Please enter a valid Email Address or Phone Number"
+    : "";
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Container
+      component="main"
+      maxWidth="xs"
+      onKeyPress={(event) => {
+        if (event.key === "Enter") {
+          handleSubmit(event);
+        }
+      }}
+    >
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5" gutterBottom>
+          Astoria Mutual Aid Network Volunteer Portal
+        </Typography>
+        <br />
+        <Typography component="h2" gutterBottom>
+          Enter your Email Address or Phone Number below to receive your login
+          link
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            error={error}
+            helperText={errorMessage}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="emailOrPhoneNumber"
+            label="Email Address or Phone Number"
+            name="emailOrPhoneNumber"
+            autoFocus
+            value={value}
+            onChange={handleChange}
+          />
+          <Button
+            onClick={handleSubmit}
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            ENTER
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link
+                href="https://www.astoriamutualaid.com/give-help"
+                variant="body2"
+              >
+                {"Not a volunteer yet? Sign up here!"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}
