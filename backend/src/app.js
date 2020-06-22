a4e0309ac6e8cf7adfeb50cfcf2d01ac65c8170c
Omar De Leo <omar.deleo@gmail.com>
2020-06-21 21:17:45 -0400

    Add test route

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,8 @@ const port = 3001;
 
 const app = express();
 
+app.get('/api/v1/', (req, res) => res.json({ greeting: 'Hello World!' }))
+
 app.listen(port, host, () =>
   console.log(`Example app listening at http://${host}:${port}`)
 );
