91c68033e461e154912c717d964de302f0474e3b
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-06-28 12:58:48 -0400

    removed un-used route

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,12 @@ const { verifyToken } = require('./token');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('ayooo');
-});
-
 app.get('/auth/', (req, res) => {
   //   console.log(verifyToken(req.query.token));
   if (!verifyToken(req.query.token)) res.sendStatus(401);
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log('Listening on 3000');
+app.listen(3001, () => {
+  console.log('Listening on 3001');
 });
