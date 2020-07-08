6e30058520b82f968eb92e814f36c5949c9d30b4
Omar De Leo <omar.deleo@gmail.com>
2020-07-08 17:23:43 -0400

    Create MultipleSelect component

---
+++ frontend/src/MultipleSelect.js
@@ -0,0 +1,30 @@
+import React from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Checkbox from "@material-ui/core/Checkbox";
+import Chip from "@material-ui/core/Chip";
+import MultipleSelectDropdown from "./MultipleSelectDropdown";
+
+export default function MultipleSelect(props) {
+  const { values, showAs } = props;
+
+  let display;
+
+  if (showAs === "Dropdown") {
+    display = <MultipleSelectDropdown options={values} />;
+  }
+
+  if (showAs === "List") {
+    let options = values.map(value => {
+      return (
+        <ListItem key={value}>
+          <Checkbox />
+          <Chip size="small" label={value} />
+        </ListItem>
+      );
+    });
+    display = <List>{options}</List>;
+  }
+
+  return display;
+}
