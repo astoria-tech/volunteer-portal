7ce5f15f2d186851ccb9583baee89bf8a3833c63
Omar De Leo <omar.deleo@gmail.com>
2020-07-08 17:23:06 -0400

    Create MultipleSelectDropdown component

---
+++ frontend/src/MultipleSelectDropdown.js
@@ -0,0 +1,88 @@
+import React from "react";
+import "./MultipleSelectDropdown.css";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import Chip from "@material-ui/core/Chip";
+
+export default function MultiSelectDropdown(props) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [menuOptions, _setMenuOptions] = React.useState([...props.options]);
+  const [selectedOptions, setSelectedOptions] = React.useState([]);
+
+  const menuRef = React.useRef(menuOptions);
+
+  const setMenuOptions = data => {
+    menuRef.current = data;
+    _setMenuOptions(data);
+  };
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleSelection = option => {
+    const newData = menuOptions.filter(opt => opt !== option);
+    setMenuOptions(newData);
+    const newOptions = selectedOptions.concat([option]);
+    setSelectedOptions(newOptions);
+    setAnchorEl(null);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleDelete = option => {
+    const newMenuOptions = menuOptions.concat([option]).sort();
+    setMenuOptions(newMenuOptions);
+    const newSelectedOptions = selectedOptions
+      .filter(opt => {
+        return opt !== option;
+      })
+      .sort();
+    setSelectedOptions(newSelectedOptions);
+  };
+
+  return (
+    <div className="multiselect-dropdown">
+      <Button
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        Select an option
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {menuOptions.map(opt => {
+          return (
+            <MenuItem
+              key={opt}
+              className="menu-item"
+              onClick={() => handleSelection(opt)}
+            >
+              <Chip key={opt} className="menu-chip" label={opt} size="small" />
+            </MenuItem>
+          );
+        })}
+      </Menu>
+      <div>
+        {selectedOptions.map(option => (
+          <Chip
+            key={option}
+            className="selectedChip"
+            label={option}
+            size="small"
+            onDelete={() => handleDelete(option)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
