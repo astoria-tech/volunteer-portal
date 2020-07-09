be42def8a38f447d0604d3b77a580be18fa8198f
Omar De Leo <omar.deleo@gmail.com>
2020-07-08 23:06:57 -0400

    Render volunteer form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 import SignIn from './SignIn';
+import Form from './Form';
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      <Form />
     </div>
   );
 }
