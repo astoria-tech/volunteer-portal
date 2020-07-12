0f685e435861b354ed8628bff300eec918c7c6f3
Omar De Leo <omar.deleo@gmail.com>
2020-07-11 20:18:50 -0400

    Create formatData util function to format form data

---
+++ frontend/src/utils/formUtils.js
@@ -0,0 +1,24 @@
+export const formatData = (data, questions) => {
+  const formattedData = {};
+  for (let idx of Object.keys(data)) {
+    const question = questions[idx];
+    if (question) {
+      formattedData[question.fieldName] = data[idx];
+    } else {
+      const [questionIdx, choiceIdx] = idx.split("-");
+      const { fieldName, possibleValues } = questions[questionIdx]
+      const answer = possibleValues[choiceIdx]
+      if (!formattedData[fieldName]) {
+        if (data[idx]) {
+          formattedData[fieldName] = [answer];
+        }
+      } else {
+        if (data[idx]) {
+          const newValue = formattedData[fieldName].concat([answer])
+          formattedData[fieldName] = newValue;
+        }
+      }
+    }
+  };
+  return formattedData;
+}
