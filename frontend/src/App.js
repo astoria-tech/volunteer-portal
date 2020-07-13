9ad52f023a1a54e8829751b5549522e73c6972d0
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-07-13 18:56:39 -0400

    updated React to redirect to form when token is present. Also set generated link which creates cookie to redirect to localhost:3000 after cookie is created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 import SignIn from './SignIn';
 import Form from './Form';
 
 function App() {
+  const [token,setToken]=useState(false)
+  useEffect(()=>{
+    fetch('/api/v1/initial')
+    .then(res=>{
+      if(res.status===302){
+        setToken(true)
+      }
+    })
+  },[]); 
   return (
     <div className="App">
-      <SignIn />
+      {token ? <Form />:<SignIn />}
     </div>
   );
 }
