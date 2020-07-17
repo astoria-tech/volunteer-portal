2d44458ff60e4d4f42a99f95c5fc8b031fa854e6
Peter Valdez <peter@shipyard.build>
2020-07-17 19:53:45 -0400

    Load JWT keys from environment and add other environment variables to Docker Compose file

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 
 const config = {
-  baseId: process.env.AIRTABLE_BASE_ID,
+  AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
   AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
   AIRTABLE_VOLUNTEERS_TABLE_NAME:
     process.env.AIRTABLE_VOLUNTEERS_TABLE_NAME || 'Volunteers',
   AIRTABLE_VOLUNTEERS_VIEW_NAME:
     process.env.AIRTABLE_VOLUNTEERS_VIEW_NAME || 'Grid view',
   CRYPTO_KEY: process.env.CRYPTO_KEY,
+  JWT_PUBLIC_KEY_BASE64: process.env.JWT_PUBLIC_KEY_BASE64,
+  JWT_PRIVATE_KEY_BASE64: process.env.JWT_PRIVATE_KEY_BASE64
 };
 
 module.exports = config;
