205f1b63ed73fa1a03fd427709faa51acf547eed
Omar De Leo <omar.deleo@gmail.com>
2020-08-02 19:15:04 -0400

    Check for token the first time the app loads

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,16 @@ import "./App.css";
 
 function App() {
   const [token, setToken] = useState(false);
+  const [tokenChecked, setTokenChecked] = useState(false)
   useEffect(() => {
-    fetch("/api/v1/initial").then((res) => {
-      if (res.status === 202) {
-        setToken(true);
-      }
-    })
+    if (!tokenChecked) {
+      fetch("/api/v1/initial").then((res) => {
+        if (res.status === 202) {
+          setToken(true);
+        }
+      });
+      setTokenChecked(true);
+    }
   }, []); 
   return (
     <div className="App">
